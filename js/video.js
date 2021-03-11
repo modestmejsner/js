e52d89a2af78df728eca15f050a7ab1b7993677a
modestmejsner <modestmejsner@gmail.com>
2021-03-11 12:16:30 +0100

    Variables and scope

--- js/video.js
+++ js/video.js
@@ -1,4 +1,53 @@
-document.getElementById("button").onclick = function() {
-    document.getElementById("confirm").innerHTML = "Order placed. Check email for confirmation";
-    document.getElementById("button").style.display = "none";
+// // document.getElementById("button").onclick = function() {
+// //     document.getElementById("confirm").innerHTML = "Order placed. Check email for confirmation";
+// //     document.getElementById("button").style.display = "none";
+// // }
+// var username = prompt();
+// alert(username);
+// console.log(username);
+// /* 
+// Line1
+// Line2
+// */
+// console.log(username);
+
+// (function () {
+//     //IIFE Inmidiete invoke function expression
+//     // age = 5 without var szuka w 1 lini wyżej i nie znajduje to tworzy w 1 wyzej czyli global
+//     function test() {
+//         var age = 1;
+//         function test1() {
+//             age = 5   
+//            } 
+//            test1();
+//     } 
+// test();
+// })();
+
+// (function (params) {
+    
+// })();
+
+// var y = 10; //global but in window
+// let x = 10; //global without window better to use let
+
+// {
+// //block
+//     let z = 10;
+//     console.log(z*z);
+//     var k = 10000;
+//     let x = 5;
+//     console.log(x);
+// }
+// console.log(x);
+
+{
+ let x = 5;
+ let b = "10";
+ x = x + b;
+ console.log(x);
+
 }
+
+//primitives
+//object
