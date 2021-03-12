f91b7cd09a98c88738428031dcdd8fbf947258d3
modestmejsner <modestmejsner@gmail.com>
2021-03-12 13:53:07 +0100

    objects and primitives

--- js/video.js
+++ js/video.js
@@ -70,31 +70,121 @@
 
 // let now = new Date;
 // let grades = ["A","B","C",'D','E','F'];
+// {
+
+//     let myName = "Modest"; //primitives
+
+//     myName = new String(myName); //seriosly? WOW- BOXING
+//     myName = myName.toUpperCase(); //boxed to object
+//     myName = myName.valueOf(); //bierze tylko value i przenosi czyli UNBOXING
+    
+//     console.log(typeof(myName));
+
+
+//     let yourName = new String("Pati");
+//     yourName = yourName.valueOf();
+//     console.log(typeof(yourName));
+
+
+// //     console.log(typeof(myName.toUpperCase())); // object
+// //     myName = myName.toUpperCase(); //back to primitives
+// //     console.log(typeof(myName)); // string
+    
+// //     let yourName = new String("Patrycja"); //object creation
+// //   //  yourName.valueOf(); //returns object type
+// //     let youPrimitiveName = yourName.toUpperCase();
+// //     console.log(typeof(youPrimitiveName));
+// //     console.log(typeof(yourName)); //write to console object
+
+
+// }
+
 {
+    // let number = 9007199254740991;
+    // console.log(Number.isSafeInteger(number))
 
-    let myName = "Modest"; //primitives
+    // let number =  -9007199254740991;
+    // console.log(Math.pow(number,101)); 
+    // console.log(1/0); 
+    // number = 5.5;
+    // console.log(number); 
 
-    myName = new String(myName); //seriosly? WOW- BOXING
-    myName = myName.toUpperCase(); //boxed to object
-    myName = myName.valueOf(); //bierze tylko value i przenosi czyli UNBOXING
+
+    // let slicesPizza = 10;
+    // // console.log(slicesPizza % 4.25); //WOW feature modulo decimal value
+    // // slicesPizza++;     
+    // // console.log(slicesPizza); 
+    // // ++slicesPizza;
+    // // console.log(slicesPizza);
+
+    // // let newSliecesPizza = --slicesPizza; //++sliecesPizza jest najpierw zincrementowana a pozniej przypisana
+    // // let newnewSliecesPizza = slicesPizza--; //sliecesPizza++ jest najpierw przypidana ze starą wartością do nowej a pozniej zincrementowana w sobie samej jedynie
     
-    console.log(typeof(myName));
+    // // console.log(newSliecesPizza);
+    // // console.log(newnewSliecesPizza);
+    // slicesPizza *= 5;
+    // console.log(slicesPizza)
 
+    // let x =5;
+    // let y ="10.9999 plus something";
+    // //y = Number(y); //.parseInt(y)
+    // x = Number.parseInt(y) //gives a 10
+    // z= Number.parseFloat(y) //gives a 10.999
+    // console.log(x+z);
 
-    let yourName = new String("Pati");
-    yourName = yourName.valueOf();
-    console.log(typeof(yourName));
+    // let input = prompt("Put any number");
+    // console.log("Decimal: " + input);
+    // console.log("Binary: " + Number.parseInt(input,2));
+    // console.log("Octal: " + Number.parseInt(input,8));
+    // console.log("Hex: " + Number.parseInt(input,16));
 
+    // let input = Number(prompt("Put decimal number"));
+    // console.log("Binary: " + input.toString(2));
+    // console.log("Octal: " + input.toString(8));
+    // console.log("Hex: " + input.toString(16));
 
-//     console.log(typeof(myName.toUpperCase())); // object
-//     myName = myName.toUpperCase(); //back to primitives
-//     console.log(typeof(myName)); // string
+    // let myname = "Modest";
+    // console.log(`My name is  ${myname}`);
+
+    // let test = "Very long string adhlasdhlakshdl laksdl ksdljk lakjsdlaksjd lajsjdl akjsdlkajdlasdjkla." //or toggle word wrap
+
+    // console.log(test);
+
+    // var favFood = "tackos";
+    // console.log(favFood.concat(" los pornox"));
+    // console.log(favFood);
+
+    // let content = " bla bla number bla bla";
+    // search1 = "number";
+    // search2 = "string";
+
+    // console.log(content.includes(search2));
+
+    //     let content = " bla bla number bla bla number";
+    // search1 = "number";
+    // search2 = "string";
+
+    // console.log(content.indexOf(search1,10));
+    // console.log(content.lastIndexOf(search1,10));
+
+    // console.log(content.split(" "));
     
-//     let yourName = new String("Patrycja"); //object creation
-//   //  yourName.valueOf(); //returns object type
-//     let youPrimitiveName = yourName.toUpperCase();
-//     console.log(typeof(youPrimitiveName));
-//     console.log(typeof(yourName)); //write to console object
 
+    let position = {
+        x: 10,
+        y: 20
+    }
+    let k = "string";
+    let l = k;
+    //primitives copied its values to the next variable
 
+    let myPosition = position; //point to the same object any changes reflect in both
+    console.log(position);
+    console.log(myPosition);
+
+    position.x = 100;
+    console.log(position);
+    console.log(myPosition);
 }
+
+
